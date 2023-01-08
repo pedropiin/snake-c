c2f1f6993b3bedfd4bb7ecf06ac4b2c3000d4ee5
Pedro da Rosa Pinheiro <p231081@dac.unicamp.br>
2023-01-07 21:26:32 -0300

    tabuleiro pronto

--- snake.c
+++ snake.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
+
+#define COLS 60
+#define ROWS 30
+
+void printa_tabuleiro() {
+    printf("┌");
+    for (int i = 0; i < COLS; i++) {
+        printf("─");
+    }
+    printf("┐\n");
+
+    for (int j = 0; j < ROWS; j++) {
+        printf("|");
+        for (int i = 0; i < COLS; i++) {
+            printf("·");
+        }
+        printf("|\n");
+    }
+
+    printf("└");
+    for (int i = 0; i < COLS; i++) {
+        printf("─");
+    }
+    printf("┘\n");
+}
+
+void cria_nova_maca(int *xmaca, int *ymaca) {
+    xmaca = rand() % COLS;
+    ymaca = rand() % ROWS;
+}
 
 int main() {
-    
+    int quit = 0;
+    while (!quit) {
+        printa_tabuleiro();
+    }
+
     return 0;
 }
